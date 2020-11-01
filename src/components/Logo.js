440e4525492f702231ca7b98014c709273799e66
Jon Portella <jportella93@gmail.com>
2020-11-01 13:32:42 +0100

    chore: remove useless constructor

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,6 @@
 import React from 'react'
 
 const Logo = class extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
   render() {
     return (
       <h1 style={{color: 'rgb(255,68,0)'}}>RECETAS QUICK</h1>
